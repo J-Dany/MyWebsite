5e64ac20ca95e3edc983649aee03bf924d1217f7
J-Dany <jdanya@outlook.com>
2022-06-18 15:43:47 +0200

    send data as formdata

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 import Swiper, { Autoplay } from 'swiper';
 
 const btnSubmitFormContact = document.getElementById("submitFormContact");
+const btnOpenModal = document.getElementById("btnOpenModal");
+const successMessage = document.getElementById("successMessage");
+
+btnOpenModal.addEventListener("click", () => {
+    if (successMessage.classList.contains("d-block")) 
+    {
+        successMessage.classList.remove("d-block");
+        successMessage.classList.add("d-none");
+    }
+});
 
 btnSubmitFormContact.addEventListener("click", () => {
     const form = document.forms["formContact"];
@@ -11,20 +21,26 @@ btnSubmitFormContact.addEventListener("click", () => {
         return;
     }
 
+    const formData = new FormData();
+    formData.append("from", form.from.value);
+    formData.append("contact", form.contact.value);
+    formData.append("request", form.request.value);
+    formData.append("body", form.body.value);
+
     try
     {
-        const subject = `From: ${form.from.value}`;
-        const body = `
-<h4>Request: ${form.request.value}</h4>
-
-<p>${form.body.value}</p>
-`;
-        window.open(`mailto:castiglia.daniele@outlook.com?subject=${subject}&body=${body}`);
+        fetch("inserisci_richiesta.php", {
+            method: "POST",
+            body: formData
+        });
     }
     catch (ex)
     {
         alert("Error on sending email!");
     }
+
+    successMessage.classList.add("d-block");
+    successMessage.classList.remove("d-none");
 
     form.reset();
 });
